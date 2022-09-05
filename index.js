5b43896f000fe7f28bf78dd3802f3a49575be880
Lolikana <thomas.guiart@gmail.com>
2022-09-05 13:28:06 +0900

    modify workDayBoxClass class

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function f(email, password) {
 
   let i = 1;
   for (i; i < 10; i++) {
-    if ((await workDayBoxClass(i)) === 'day work') break;
+    if ((await workDayBoxClass(i)) === 'day') break;
   }
 
   await driver
